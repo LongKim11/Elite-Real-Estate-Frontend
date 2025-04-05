14846769865b200920b53742f8d794fe4052f770
Long Vo <vokimlong11.cs@gmail.com>
2025-04-05 09:26:31 +0700

    Fix navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <nav className="flex h-[100px] items-center justify-between">
             <div className="flex flex-[3] items-center gap-16">
@@ -13,42 +15,63 @@ export const Navbar = () => {
                 </a>
                 <a
                     href="/"
-                    className="font-semibold transition duration-200 hover:text-yellow-500"
+                    className="hidden font-semibold transition duration-200 hover:text-yellow-500 md:flex"
                 >
                     Home
                 </a>
                 <a
                     href="/"
-                    className="font-semibold transition duration-200 hover:text-yellow-500"
+                    className="hidden font-semibold transition duration-200 hover:text-yellow-500 md:flex"
                 >
                     About
                 </a>
                 <a
                     href="/"
-                    className="font-semibold transition duration-200 hover:text-yellow-500"
+                    className="hidden font-semibold transition duration-200 hover:text-yellow-500 md:flex"
                 >
                     Contact
                 </a>
                 <a
                     href="/"
-                    className="font-semibold transition duration-200 hover:text-yellow-500"
+                    className="hidden font-semibold transition duration-200 hover:text-yellow-500 md:flex"
                 >
                     Agents
                 </a>
             </div>
-            <div className="flex h-full flex-[2] items-center justify-end bg-[#fcf5f3]">
+            <div className="flex h-full flex-[2] items-center justify-end bg-transparent md:bg-[#fcf5f3]">
                 <a
                     href="/"
-                    className="m-5 px-6 py-3 font-semibold transition-all duration-200 hover:scale-105"
+                    className="m-5 hidden px-6 py-3 font-semibold transition-all duration-200 hover:scale-105 md:flex"
                 >
                     Sign In
                 </a>
                 <a
                     href="/"
-                    className="m-5 rounded bg-[#fece51] px-6 py-3 font-semibold transition-all duration-200 hover:scale-105"
+                    className="m-5 hidden rounded bg-[#fece51] px-6 py-3 font-semibold transition-all duration-200 hover:scale-105 md:flex"
                 >
                     Sign Up
                 </a>
+                <div className="flex md:hidden">
+                    <img
+                        src="/menu.png"
+                        className="z-50 h-9 w-9 cursor-pointer"
+                        onClick={() => {
+                            setOpen((prev) => !prev);
+                        }}
+                    ></img>
+                </div>
+                <div
+                    className={`absolute top-0 flex h-screen w-1/3 flex-col items-center justify-center gap-10 bg-black text-2xl text-white transition-all duration-500 ease-in-out md:hidden ${
+                        open ? 'right-0' : '-right-full'
+                    }`}
+                >
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sign In</a>
+                    <a href="/">Sign Up</a>
+                </div>
             </div>
         </nav>
     );
